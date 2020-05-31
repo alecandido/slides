461c91b37d83d3f5cfdd6e58db44bdcadc9de499
AleCandido <candido.ale@gmail.com>
2020-05-31 12:53:56 +0200

    Template engine working, and it's one

--- reveal/templates/make-scripts/make.py
+++ reveal/templates/make-scripts/make.py
@@ -9,10 +9,13 @@ customization.
 import pathlib
 import shutil
 import json
+import sys
+import subprocess
 import logging
 import logging.config
 
 import toml
+import click
 
 
 def main(here, root):
@@ -68,17 +71,41 @@ def main(here, root):
     # add structures
     # --------------
 
-    # MISSING
+    structure = root / "structures" / configs["structure"]["name"]
+    requirements = structure / "requirements.txt"
+    structure_configs = structure / "structure.yaml"
+    template = structure / "template.html"
+    build_script = structure / "build_template.py"
+
+    if click.confirm(
+        "Do you want to install the required dependencies?", default=False
+    ):
+        print("Installing required packages:")
+        with open(requirements) as f:
+            for line in f.readlines():
+                if not line or line[0] == "#":
+                    continue
+                print(f"  - {line}")
+
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "-r", str(requirements.resolve())]
+        )
+        # TODO add logs
+
+    # TODO manage configs
+
+    shutil.copy2(build_script, my_reveal)
+    shutil.copy2(template, my_reveal)
+
+    # TODO hardcoded files tree, this should depend on the structure chosen
+    slides = my_reveal / "src" / "slides"
+    slides.mkdir(parents=True, exist_ok=True)
+    (slides / "1.html").touch(exist_ok=True)
 
     # add themes
     # ----------
 
     # MISSING
-
-    # add src folder
-    # --------------
-
-    (my_reveal / "src").mkdir(exist_ok=True)
 
 
 if __name__ == "__main__":
