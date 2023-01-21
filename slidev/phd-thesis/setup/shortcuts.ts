02fd0b700df85d3a4355d9c132df0ac274341002
Alessandro Candido <candido.ale@gmail.com>
2023-01-21 17:27:05 +0100

    Customize shortcuts

---
+++ slidev/phd-thesis/setup/shortcuts.ts
@@ -0,0 +1,58 @@
+import type { NavOperations, ShortcutOptions } from '@slidev/types'
+import { defineShortcutsSetup } from '@slidev/types'
+
+
+export default defineShortcutsSetup((nav: NavOperations, base: ShortcutOptions[]) => {
+  console.log(base)
+  return [
+    ...base.filter(
+      map => !["goto", "prev_left", "next_right"].includes(map.name)
+    ),
+    {
+      key: 'h',
+      fn: () => nav.prev(),
+      autoRepeat: true,
+    },
+    {
+      key: 'l',
+      fn: () => nav.next(),
+      autoRepeat: true,
+    },
+    {
+      key: 'k',
+      fn: () => nav.prevSlide(),
+      autoRepeat: true,
+    },
+    {
+      key: 'j',
+      fn: () => nav.nextSlide(),
+      autoRepeat: true,
+    },
+    {
+      key: 'g',
+      fn: () => nav.goFirst(),
+      autoRepeat: true,
+    },
+    {
+      key: 'shift+g',
+      fn: () => nav.goLast(),
+      autoRepeat: true,
+    },
+    {
+      key: 't',
+      fn: () => nav.showGotoDialog(),
+      autoRepeat: true,
+    },
+    {
+      key: 'b',
+      fn: () => nav.go(2),
+      autoRepeat: true,
+    },
+    {
+      key: 'e',
+      fn: () => nav.go(27),
+      autoRepeat: true,
+    },
+  ]
+})
+
