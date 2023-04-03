be59c7388ae94eaf6dd29ecd4805300f3a612596
Alessandro Candido <candido.ale@gmail.com>
2023-04-03 14:30:14 +0200

    Init IFAE slides

---
+++ slidev/2023-IFAE/components/CiteArxiv.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export default {
+    props: {
+      aref: String
+    }
+  }
+</script>
+
+<template>
+  <a class="arxiv-ref" :href="'https://arxiv.org/abs/' + aref">
+    <code>
+      [arXiv: {{ aref }}]
+    </code>
+  </a>
+</template>
+
+<style>
+  a.arxiv-ref {
+    border-style: none !important;
+  }
+
+  a.arxiv-ref code {
+    @apply bg-zinc-200 dark-bg-zinc-800
+    @apply c-green-600 dark-c-green-500
+
+  }
+
+  a.arxiv-ref code:hover {
+    @apply c-green-700 dark-c-green-300
+  } 
+</style>
+
+
---
+++ slidev/2023-IFAE/layouts/section.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="slidev-layout section w-full h-full grid">
+    <div class="my-auto text-center">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+  .slidev-layout.section h1 {
+    font-size: 2rem !important;
+    margin: 0;
+  }
+</style>
